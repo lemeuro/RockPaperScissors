c4d4adb2182e89bf5bf8c93a0db63d27cd3ce8f1
Lem Euro <mr.lemeuro@gmail.com>
2022-07-15 17:30:47 +0300

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Lem Euro on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
